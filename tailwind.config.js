5df3720f2d5ee58804e085ecd677d47db39619ca
escojwd <junior19941703@gmail.com>
2021-04-12 18:12:11 +0400

    header

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#e9204f',
+        secondary: '#1b1b1b',
+      },
+      fontFamily: {
+        mainFont: ['Montserrat']
+      },
+      width: {
+        customOne: '40rem',
+        customTwo: '27rem',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
