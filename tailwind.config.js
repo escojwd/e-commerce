03814a4ccd7d112098544a28e6e6c5c4b65b9d36
escojwd <junior19941703@gmail.com>
2021-04-13 03:06:10 +0400

    services, works styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,13 @@ module.exports = {
       colors: {
         primary: '#e9204f',
         secondary: '#1b1b1b',
+        third: '#f3f3f3',
       },
       fontFamily: {
         mainFont: ['Montserrat']
       },
       width: {
+        container: '65%',
         customOne: '40rem',
         customTwo: '27rem',
       }
